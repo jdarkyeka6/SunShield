54a565f73e0d7b1e8e077ad044b84ce41d1d1ea9
Jake D <jakedryka1@gmail.com>
2026-09-02 20:06:04 +0800

    Add native iOS App Store project
    * Configure Capacitor iOS dependencies
    
    * Enable static export for iOS
    
    * Add Sun Shield Capacitor config
    
    * Support iPhone safe-area viewport
    
    * Ignore generated web build files
    
    * Document iOS App Store build path
    
    * Add automated native iOS project generation
    
    * Generate iOS App Store project
    
    * Bundle web assets inside generated iOS app
    
    * Generate iOS App Store project
    
    ---------
    
    Co-authored-by: SunShield iOS Builder <actions@users.noreply.github.com>

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jake.sunshield',
+  appName: 'Sun Shield',
+  webDir: 'out'
+};
+
+export default config;
---
+++ ios/App/App/public/_next/static/chunks/app/layout-062424f194610830.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1290:()=>{},5270:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,1290,23)),Promise.resolve().then(n.t.bind(n,5603,23))},5603:()=>{}},e=>{e.O(0,[984,441,255,358],()=>e(e.s=5270)),_N_E=e.O()}]);
